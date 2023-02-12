58c205b064f9c130b59ee5341196b8fccab462e7
KanstantsinS <kosta232mentorweb@gmail.com>
2023-02-12 09:12:19 +0300

    persons.js

---
+++ js/persons.js
@@ -0,0 +1,44 @@
+export default
+{
+   // DV:
+   "SEREBRIAKOV": 1,
+   "ELENA_ANDREEVNA": 2,
+   "SONYA": 3,
+   "VOINITSKAJA": 4,
+   "VOINITSKIJ": 5,
+   "ASTROV": 6,
+   "TELEGIN": 7,
+   "MARINA": 8,
+   "RABOTNIK": 9,
+   "STOROZH": 10,
+   
+   // authors:
+   "CHEHOV": 11,
+
+   "STANISLAVSKIJ": 12,
+   "CHUKOVSKIJ": 13,
+   "BUNIN": 14,
+   "KOROLENKO": 15,
+   "BRAT_CHEHOVA": 16,
+   "ZHENA_CHEHOVA": 17,
+   "GORKIJ": 18,
+
+   // VS:
+   "RANEVSKAJA": 21,
+   "ANJA":22,
+   "VARJA": 23,
+   "GAEV": 24,
+   "LOPAHIN": 25,
+   "TROFIMOV": 26,
+   "SIMEONOV": 27,
+   "SHARLOTTA": 28,
+   "EPIHODOV": 29,
+   "DUNYASHA": 30,
+   "FIRS": 31,
+   "YASHA": 32,
+   "PROHOZHIJ": 33,
+   "NACHALNIK_STANCII": 34,
+   "POCHTOVYJ_CHINOVNIK": 35,
+   "GOSTI_PRISLUGA": 36,
+
+}
