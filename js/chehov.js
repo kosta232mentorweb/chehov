b1c88414bf54780b459afbad001979ea995541e1
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-13 14:24:36 +0300

    white offscreen, start script, blakc logo offscreen

--- js/chehov.js
+++ js/chehov.js
@@ -1,1 +1,23 @@
-console.log('Проект Чехов');
+console.log( 'Проект Чехов' );
+
+// const bsOffcanvas = new bootstrap.Offcanvas('#myOffcanvas');
+
+const offcanvasElementList = document.querySelectorAll( '.offcanvas' );
+const offcanvasList = [ ...offcanvasElementList ].map( offcanvasEl => new bootstrap.Offcanvas( offcanvasEl ) );
+
+console.log( offcanvasList );
+
+setTimeout( () => {
+   if ( !doNotShowForTeachers ) offcanvasList[ 1 ].show();
+}, 500 )
+
+const doNotShowForTeachersElement = document.querySelector( '#doNotShowForTeachers' );
+let doNotShowForTeachers = Boolean(Number(localStorage.getItem( 'doNotShowForTeachers' )));
+console.log(doNotShowForTeachers);
+doNotShowForTeachersElement.checked = doNotShowForTeachers;
+
+doNotShowForTeachersElement.addEventListener( 'input', event => {
+   console.log( event.target.checked );
+   doNotShowForTeachers = event.target.checked;
+   localStorage.setItem( 'doNotShowForTeachers', Number(doNotShowForTeachers) )
+} )
