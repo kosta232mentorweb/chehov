eca1ab1684b2422156db1357f78bfe5489154a04
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-16 17:59:53 +0300

    VS-23 input state

--- js/VS-23/vs-23.js
+++ js/VS-23/vs-23.js
@@ -5,6 +5,7 @@ const img = document.querySelector( 'img#map' );
 console.log( img );
 
 img.addEventListener( 'click', e => {
+	document.querySelector('input[type="submit"]').disabled = false;
 	console.log( e );
 	console.log( e.offsetX );
 	console.log( e.offsetY );
@@ -20,6 +21,19 @@ img.addEventListener( 'click', e => {
 	circle.style.top = e.offsetY - 10 + 'px';
 
 } );
+
+document.querySelector( 'form[name="chehov-form-vs-23"]' ).addEventListener( 'submit', e => {
+	console.log( e );
+	console.log( coords.value );
+
+	const a = coords.value.split( ', ' );
+	console.log( a );
+
+	if ( isNaN( Number( a[ 0 ] ) ) || isNaN( Number( a[ 1 ] ) ) ) {
+		e.preventDefault();
+		coords.style.border = '2px solid red';
+	}
+} )
 
 const coords = document.querySelector( '[name="coords"]' );
 console.log( coords );
