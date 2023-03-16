f05eb68227906d60fde93ce9421dd44c5fe8a8f6
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-16 22:35:29 +0300

    VS-23 multiple

--- js/VS-23/vs-23.js
+++ js/VS-23/vs-23.js
@@ -2,19 +2,17 @@ console.log( 'vs-23' );
 
 const img = document.querySelector( 'img#map' );
 
-console.log( img );
-
 img.addEventListener( 'click', e => {
-	document.querySelector('input[type="submit"]').disabled = false;
-	console.log( e );
-	console.log( e.offsetX );
-	console.log( e.offsetY );
-	coords.value = ( e.offsetX / e.target.width * 100 ).toFixed() + ', ' + ( e.offsetY / e.target.height * 100 ).toFixed();
+	document.querySelector( 'input[type="submit"]' ).disabled = false;
 
-	let circle = document.querySelector( '.circle' );
+	const id = document.querySelector( 'input[type="radio"]:checked' ).id.split( 'r' )[ 1 ];
+
+	coords[ id - 1 ].value = ( e.offsetX / e.target.width * 100 ).toFixed() + ', ' + ( e.offsetY / e.target.height * 100 ).toFixed();
+
+	let circle = document.querySelector( '.circle-' + id );
 	if ( !circle ) {
 		circle = document.createElement( 'div' );
-		circle.classList.add( 'circle' );
+		circle.classList.add( 'circle', 'circle-' + id );
 		img.after( circle );
 	}
 	circle.style.left = e.offsetX - 10 + 'px';
@@ -23,17 +21,16 @@ img.addEventListener( 'click', e => {
 } );
 
 document.querySelector( 'form[name="chehov-form-vs-23"]' ).addEventListener( 'submit', e => {
-	console.log( e );
-	console.log( coords.value );
 
-	const a = coords.value.split( ', ' );
-	console.log( a );
+	document.querySelectorAll('[type="radio"]').forEach(el=>el.remove());
+	// const a = coords.value.split( ', ' );
+	// console.log( a );
 
-	if ( isNaN( Number( a[ 0 ] ) ) || isNaN( Number( a[ 1 ] ) ) ) {
-		e.preventDefault();
-		coords.style.border = '2px solid red';
-	}
+	// if ( isNaN( Number( a[ 0 ] ) ) || isNaN( Number( a[ 1 ] ) ) ) {
+	// 	e.preventDefault();
+	// 	coords.style.border = '2px solid red';
+	// }
 } )
 
-const coords = document.querySelector( '[name="coords"]' );
-console.log( coords );
+const coords = document.querySelectorAll( '[name*="coords"]' );
+// console.log( coords );
