ed15037d6e97d1be794a2aceec4231abd425f922
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-17 13:56:23 +0300

    VS-1 input

--- js/VS-1/vs-1.js
+++ js/VS-1/vs-1.js
@@ -2,6 +2,11 @@
 
 console.log( 'vs-1' );
 
+document.forms[ 0 ].querySelectorAll( 'input[type="text"]' ).forEach( el => el.addEventListener( 'input', event => {
+	event.target.value = event.target.value.at( -1 );
+} )
+)
+
 document.forms[ 0 ].addEventListener( 'submit', event => {
 	console.log( 'submit' );
 
@@ -9,14 +14,14 @@ document.forms[ 0 ].addEventListener( 'submit', event => {
 
 	const NUMBER_OF_WORDS = 15;
 
-	const res = [null];
+	const res = [ null ];
 
 	for ( let i = 1; i <= NUMBER_OF_WORDS; i++ ) {
 
 		const className = 'word' + String( i ).padStart( 2, '0' );
 		const words = document.querySelectorAll( '[class*="' + className + '"]' );
 
-		console.log( '.word' + String( i ).padStart( 2, '0' ), words );
+		// console.log( '.word' + String( i ).padStart( 2, '0' ), words );
 
 		const result = [ ...words ].reduce( ( acc, el ) => {
 			// const wIdx = Number( el.className.split( 'word' )[ 1 ].split( '-' )[ 0 ] );
@@ -30,7 +35,24 @@ document.forms[ 0 ].addEventListener( 'submit', event => {
 		res.push( result );
 	}
 
-	console.log(res);
+	console.log( res );
+
+	const resWords = res.reduce( ( acc, w, idx ) => {
+		if ( w ) {
+			acc[ idx ] = w.reduce( ( acc2, el, idx2 ) => {
+				return acc2 + document.querySelector( '.word' + String( idx ).padStart( 2, '0' ) + '-' + idx2 ).value;
+			}, '' );
+		}
+		return acc;
+
+	}, [] );
+
+	console.log( resWords );
+
+	// for ( let i = 1; i <= NUMBER_OF_WORDS; i++ ) {
+	// 	for (let )
+	// }
+
 
 
 	// 	const w = document.querySelectorAll( '[class*="word' + i + '"]' );
