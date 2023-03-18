a04a958bb71f400bb4a6a93f5d95f97572acde5a
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-18 09:36:33 +0300

    key.js

---
+++ js/key.js
@@ -0,0 +1,9 @@
+const BookAASKeyElement = document.querySelector( '#BookAASKey' );
+
+let BookAASKey = localStorage.getItem( 'BookAASKey' ) ?? '';
+BookAASKeyElement.value = BookAASKey;
+
+BookAASKeyElement.addEventListener( 'input', event => {
+	BookAASKey = event.target.value;
+	localStorage.setItem( 'BookAASKey', BookAASKey );
+} )
