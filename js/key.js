1948cc774b7fac74b0f497c48f5b2d93091b6b07
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-18 14:37:40 +0300

    BookAAS glaz

--- js/key.js
+++ js/key.js
@@ -1,9 +1,25 @@
-const BookAASKeyElement = document.querySelector('#BookAASKey');
+const BookAASKeyElement = document.querySelector( '#BookAASKey' );
 
-let BookAASKey = localStorage.getItem('BookAASKey') ?? '';
+let BookAASKey = localStorage.getItem( 'BookAASKey' ) ?? '';
 BookAASKeyElement.value = BookAASKey;
 
-BookAASKeyElement.addEventListener('input', (event) => {
+BookAASKeyElement.addEventListener( 'input', ( event ) => {
 	BookAASKey = event.target.value;
-	localStorage.setItem('BookAASKey', BookAASKey);
-});
+	localStorage.setItem( 'BookAASKey', BookAASKey );
+} );
+
+const BookAASKeyGlazElement = document.querySelector( '#BookAASKeyGlaz' );
+
+let BookAASKeyElementTimeout;
+
+BookAASKeyGlazElement.addEventListener( 'click', event => {
+	clearTimeout( BookAASKeyElementTimeout );
+	BookAASKeyElement.type = 'text';
+	event.target.style.opacity = 0.3;
+
+	BookAASKeyElementTimeout = setTimeout( () => {
+		BookAASKeyElement.type = 'password';
+		event.target.style.opacity = '';
+
+	}, 3000 )
+} )
