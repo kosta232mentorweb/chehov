8b6135ef3e09fbac154769a41793d8c7cc5402c2
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-18 09:23:22 +0300

    save restore data update for every html

--- js/saveRestoreData.js
+++ js/saveRestoreData.js
@@ -1,24 +1,26 @@
+const htmlSuffix = location.pathname.split( '/' ).slice( -1 )[ 0 ];
+
 function saveInputs() {
-	const store = [ ...document.forms[ 0 ].querySelectorAll( 'input[type="text"]' ) ].reduce( ( acc, el ) => {
+	const store = [ ...document.forms[ 0 ].querySelectorAll( 'input[type="text"], textarea' ) ].reduce( ( acc, el ) => {
 		acc[ getDomPath( el ) ] = el.value;
 		return acc;
 	}, {} );
 
 	// console.log( store );
 
-	localStorage.setItem( 'inputsValues', JSON.stringify( store ) );
+	localStorage.setItem( 'inputsValues' + htmlSuffix, JSON.stringify( store ) );
 
 }
 
 
 let store = {};
 
-const storeString = localStorage.getItem( 'inputsValues' );
+const storeString = localStorage.getItem( 'inputsValues' + htmlSuffix );
 if ( storeString ) store = JSON.parse( storeString );
 
 // console.log( store );
 
-document.forms[ 0 ].querySelectorAll( 'input[type="text"]' ).forEach( el => {
+document.forms[ 0 ].querySelectorAll( 'input[type="text"], textarea' ).forEach( el => {
 	el.value = store[ getDomPath( el ) ] ?? '';
 } )
 
