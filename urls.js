130e01706791499fcbd704ead06beef2bbd5f424
KanstantsinS <kosta232mentorweb@gmail.com>
2023-03-21 13:09:20 +0300

    urls update index.html

--- urls.js
+++ urls.js
@@ -10,43 +10,16 @@ htmlDir.forEach( htmlFile => {
 
 	update( 'html/', htmlFile );
 
-	// let text = fs.readFileSync( './html/' + htmlFile, { encoding: 'utf8' } );
-
-	// const repl1 = text.match( /(?<=src=")(.+?)(?=")/g ).filter( el => ( el.includes( '../js/' ) || el.includes( 'images' ) ) );
-
-	// const repl2 = text.match( /(?<=href=")(.+?)(?=")/g ).filter( el => ( el.includes( '../css/' ) || el.includes( 'html' ) ) );
-
-	// const repl = [ ...repl1, ...repl2 ];
-
-	// console.log( repl );
-
-	// const r = {};
-
-	// repl.forEach( el => {
-	// 	console.log( el.split( '?' )[ 0 ] )
-	// 	console.log( el.split( '?' )[ 0 ] + '?kkey=' + Math.random() )
-	// 	r[ el.split( '?' )[ 0 ] ] = el.split( '?' )[ 0 ] + '?kkey=' + Math.random()
-	// } )
-
-	// console.log( r );
-
-	// for ( let key in r ) {
-	// 	text = text.replace( key, r[ key ] )
-	// }
-
-	// fs.writeFileSync( './distr/html/' + htmlFile, text );
-
-
 } )
 
 function update( path, fileName ) {
 	let text = fs.readFileSync( './' + path + fileName, { encoding: 'utf8' } );
 
-	const repl1 = text.match( /(?<=src=")(.+?)(?=")/g ).filter( el => ( el.includes( '../js/' ) || el.includes( 'images' ) ) );
+	const src = text.match( /(?<=src=")(.+?)(?=")/g ).filter( el => ( el.includes( '../js/' ) || el.includes( 'images' ) ) );
 
-	const repl2 = text.match( /(?<=href=")(.+?)(?=")/g ).filter( el => ( el.includes( '../css/' ) || el.includes( 'html' ) ) );
+	const href = text.match( /(?<=href=")(.+?)(?=")/g ).filter( el => ( el.includes( '../css/' ) || el.includes( 'html' ) ) );
 
-	const repl = [ ...repl1, ...repl2 ];
+	const repl = [ ...src, ...href ];
 
 	console.log( repl );
 
